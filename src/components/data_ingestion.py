e96defcd64664b58283fbb4223e43ffa7357b3b5
Lukasz Birski <lukaszbirski@Mac-Studio-Lukasz.local>
2024-05-12 22:30:53 +0200

    Data Transformation Added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,13 +5,15 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 
 @dataclass
 class DataIngestionConfig:
-    tran_data_path: str=os.path.join('artifact',"train.cvs")
-    test_data_path: str=os.path.join('artifact',"test.cvs")
-    raw_data_path: str=os.path.join('artifact',"data.cvs")
+    tran_data_path: str=os.path.join('artifacts',"train.cvs")
+    test_data_path: str=os.path.join('artifacts',"test.cvs")
+    raw_data_path: str=os.path.join('artifacts',"data.cvs")
 
 class DataIngestion:
     def __init__(self):
@@ -44,4 +46,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_tranformation=DataTransformation()
+    data_tranformation.initiate_data_transformation(train_data, test_data)
