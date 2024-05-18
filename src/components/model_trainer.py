04ed29354fcd1e9a1a4f8a1734ce58145a56a91c
Lukasz Birski <lukaszbirski@Mac-Studio-Lukasz.local>
2024-05-18 21:52:05 +0200

    Hyperparameter Training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -81,7 +81,7 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 }
             }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models, params=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,17 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         reports = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            grid_search = GridSearchCV(model, param, cv=3)
+            grid_search.fit(X_train, y_train)
+
+            model.set_params(**grid_search.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
